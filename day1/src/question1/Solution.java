ea6a677ea76d3e8f6ae569c40441048ca62e6906
v_chenshengsheng <v_chenshengsheng@dds.com>
2020-08-25 14:06:36 +0800

    #solution 1 two number sum equal target

--- day1/src/question1/Solution.java
+++ day1/src/question1/Solution.java
@@ -1,9 +1,29 @@
 package question1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * PROBLEM: 两数之和等于目标值
  * BEFORE: hash
  */
 public class Solution {
+    public int[] twoSum(int[] nums,int target){
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+
+            }
+            map.put(nums[i], i);
+
+
+        }
+        return null;
+
+
+
+    }
 
 }
